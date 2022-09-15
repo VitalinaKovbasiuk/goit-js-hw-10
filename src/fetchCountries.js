a422f1502c0a3b72e1badef7976a3c1970d29d9e
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-09-16 00:04:53 +0300

    task

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+const COUNTRU_URL = 'https://restcountries.com';
+
+const fetchCountries = function (name) {
+  return fetch(`${COUNTRU_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+  .then(
+    response => {
+      if (response.status === 404) {
+        return Promise.reject(new Error());
+      }
+      return response.json();
+    }
+  );
+};
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -71,6 +71,14 @@
 // Необхідно застосувати прийом Debounce на обробнику події і робити HTTP-запит через 300мс після того, 
 // як користувач перестав вводити текст. Використовуй пакет lodash.debounce.
 
+// lodash.debounce v4.0.8
+// Using npm:
+// $ {sudo -H} npm i -g npm
+// $ npm i --save lodash.debounce
+
+// In Node.js:
+// var debounce = require('lodash.debounce');
+
 // Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується, 
 // а розмітка списку країн або інформації про країну зникає.
 
@@ -103,5 +111,107 @@
 
 
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputBox: document.querySelector('#search-box'),
+  countriList: document.querySelector('.country-list'),
+  countriInfo: document.querySelector('.country-info'),
+};
+
+refs.inputBox.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(e) {
+  const countryName = e.target.value.trim();
+
+  if (!countryName) {
+    clearTemplate();
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(data => {
+      if (data.length > 10) {
+        specificNameInfo(Notify.info('Too many matches found. Please enter a more specific name.'));
+        clearTemplate();
+        return;
+      }
+      renderTemplate(data);
+    })
+    .catch(error => {
+      clearTemplate();
+      errorWarn(Notify.failure(`Oops, there is no country with that name`));
+    });
+}
+
+function clearTemplate() {
+    refs.countriInfo.innerHTML = '';
+    refs.countriList.innerHTML = '';
+  }
+
+function renderTemplate(elements) {
+  let template = '';
+  let refsTemplate = '';
+  clearTemplate();
+
+  if (elements.length === 1) {
+    template = createItem(elements);
+    refsTemplate = refs.countriInfo;
+  } else {
+    template = createList(elements);
+    refsTemplate = refs.countriList;
+  }
+
+  drawTemplate(refsTemplate, template);
+}
+
+function createItem(element) {
+  return element.map(
+    ({ name, capital, population, flags, languages }) =>
+      `
+      <img
+        src="${flags.svg}" 
+        alt="${name.official}" 
+        width="20" 
+        height="20">
+      <h1 class="country-info__title">${name.official}</h1>
+      <ul class="country-info__list">
+          <li class="country-info__item">
+          <span>Capital:</span>
+        ${capital}
+          </li>
+          <li class="country-info__item">
+          <span>Population:</span>
+          ${population}
+          </li>
+          <li class="country-info__item">
+          <span>Lenguages:</span>
+          ${Object.values(languages)}
+          </li>
+      </ul>
+  `
+  );
+}
+
+function createList(elements) {
+  return elements
+    .map(
+      ({ name, flags }) => `
+      <li class="country-list__item">
+        <img class="country-list__img" 
+          src="${flags.svg}" 
+          alt="${name.official}" 
+          width="20" 
+          height="20">
+        ${name.official}
+      </li>`
+    )
+    .join('');
+}
+
+function drawTemplate(refs, markup) {
+  refs.innerHTML = markup;
+}
