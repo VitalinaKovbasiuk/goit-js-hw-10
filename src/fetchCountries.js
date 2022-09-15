0485e4a2e4fcf1f5330c016f90b61aea1ee416cd
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-09-16 00:10:46 +0300

    task v.2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,10 @@
-const COUNTRU_URL = 'https://restcountries.com';
-
-const fetchCountries = function (name) {
-  return fetch(`${COUNTRU_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-  .then(
-    response => {
-      if (response.status === 404) {
-        return Promise.reject(new Error());
-      }
-      return response.json();
-    }
-  );
-};
-
-export { fetchCountries };
+export function fetchCountry(name) {  
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
+  ).then(response => {
+      if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      });
+}  
