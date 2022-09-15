d1af2708e755373333f94abe31b27248006ac94a
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-09-16 00:20:38 +0300

    task final

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,12 @@
 export function fetchCountry(name) {  
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
-  ).then(response => {
-      if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
+  )
+  .then(
+    response => {
+      if (response.status === 404) {
+        return Promise.reject(new Error());
+      }
+      return response.json();
       });
 }  
